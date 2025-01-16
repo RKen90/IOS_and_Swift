1c5ebf451ce92867e252d60ced29e31664e70206
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-16 15:10:23 +0000

    I am rich - Project 1 of IOS Developer Course

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Robert Kennard on 16/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
